1ecdd3cac573b28494887eac32df795d85be2028
isaMuneretto <you@example.com>
2023-11-28 22:00:31 -0300

    initial commit

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,29 @@
+
+
+export function Card( props ) {
+    console.log(props)
+    return (
+        <CardContainer>{/*tag section */}{/*como aqui é html e os dados dentro das tags são JS, tem que interpolar com chaves */}
+            <CardBody> {/*tag article */}
+                <div>
+                    <h2>{props.titulo}</h2>
+                    <img src={props.banner} alt="Imagem" />
+                </div>
+                <TextLimit text={props.text} limit={150} />
+            </CardBody>
+
+            <CardFooter>
+                <div>
+                    <i className="bi bi-hand-thumbs-up"></i>
+                    <span>{props.likes}</span>
+                </div>
+
+                <div>
+                    <i className="bi bi-chat"></i>
+                    <span>{props.comments}</span>
+                </div>
+
+            </CardFooter>
+        </CardContainer>
+    )
+}
